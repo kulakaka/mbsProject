313a574458eb79c4ecf3b48190d1573be3981a69
Max Ong Zong Bao <max@codesurance.com>
2022-10-04 18:46:23 +0800

    refactor: Adding of print message for redeploy.sh.

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo "Changing directory to mbsProject folder"
 cd ~/mbsProject
-git pull
+echo "Pullig git from main repo"
+git pull origin main
+echo "Deleting www folder for onepartyonembs domain"
 sudo rm -r /var/www/onepartyonembs.com.sg/html/*
+echo "Copying latest version of main branch to www folder for onepartyonembs domain"
 sudo cp ~/mbsProject/Client/* /var/www/onepartyonembs.com.sg/html/
+echo "Script has been completed!!!"
