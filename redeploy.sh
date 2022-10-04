8a4915633683188a727eb6a19148c0581a607a11
Max Ong Zong Bao <max@codesurance.com>
2022-10-04 19:24:58 +0800

    docs: Update the README.md to include redeployment script that pulls the latest version from "main" branch, rm old files and copy Client folder to www folder. Updated the redeploy.sh to include backup of db of "backup" folder.

--- redeploy.sh
+++ redeploy.sh
@@ -5,6 +5,8 @@ echo "Changing directory to mbsProject folder"
 cd ~/mbsProject
 echo "Pullig git from main repo"
 git pull origin main
+echo "Copy current version of DB to backup folder"
+sudo cp ~/mbsProject/Server/Database/mbsProject.db ~/mbsProject/backup/
 echo "Deleting www folder for onepartyonembs domain"
 sudo rm -r /var/www/onepartyonembs.com.sg/html/*
 echo "Copying latest version of main branch to www folder for onepartyonembs domain"
