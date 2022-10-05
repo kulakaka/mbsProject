382a9fef5497e65e21986ca783ee2fed20884ea6
Max Ong Zong Bao <max@codesurance.com>
2022-10-05 18:23:50 +0800

    feat: Add the sentry monitoring service.

--- Server/getData.js
+++ Server/getData.js
@@ -1,4 +1,6 @@
 const express = require ("express");
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 const bodyParser = require("body-parser");
 
 const sqlite = require("sqlite3").verbose();
@@ -15,6 +17,27 @@ const corsOptions ={
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
+Sentry.init({
+  dsn: "https://e38bdea921b04ee2b0a1d107bee4aeb0@o1208814.ingest.sentry.io/4503929787056128",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 //post request
 app.post("/api/update/:id/:selection",(req,res)=>
 {
@@ -28,7 +51,7 @@ app.post("/api/update/:id/:selection",(req,res)=>
             if(err) return console.error(err);
             console.log('Connected to database.');
         });
-        // Execute query 
+        // Execute query
         const sql = `UPDATE stuff SET SelectedSession= ? WHERE TeamMember=?`;
         db.all(sql,[selection,id],(err,data)=>{
 
@@ -46,7 +69,7 @@ app.post("/api/update/:id/:selection",(req,res)=>
           });
 
     }
-    
+
     catch(error){
         return res.json({
             status:400,
@@ -96,7 +119,7 @@ app.get("/api/info/:id",(req,res)=>{
             return res.json(rows);
 
         })
-        
+
     // Close database
         db.close((err) => {
             if (err)
@@ -105,7 +128,7 @@ app.get("/api/info/:id",(req,res)=>{
               console.log('Close the database connection.')
           });
     }
-  
+
     catch(error){
         return res.json({
             status:400,
@@ -119,9 +142,9 @@ app.get("/api/info/:id",(req,res)=>{
 app.get("/api/sms/:phno/:name/:val",(req,res)=>
 {
 
-    const accountSid = 'AC5903079836c0d20ab145562b6b5a0b41'; 
-    const authToken = 'fa8a59146c92a9f3251c2a67399ffa36'; 
-    const client = require('twilio')(accountSid, authToken); 
+    const accountSid = 'AC5903079836c0d20ab145562b6b5a0b41';
+    const authToken = 'fa8a59146c92a9f3251c2a67399ffa36';
+    const client = require('twilio')(accountSid, authToken);
     var phno = req.params.phno;
     var name = req.params.name;
     var val = req.params.val;
@@ -144,14 +167,14 @@ app.get("/api/sms/:phno/:name/:val",(req,res)=>
     "If you wish to amend the session you have selected, please click on the link below \n"+
     "https://onepartyonembs.com.sg"
 
-   
+
     try{
-        
-        client.messages.create({ 
-         body: text,  
-         messagingServiceSid: 'MG216d22c854ec229d65cd09060464e761',      
-         to: '+65'+phno 
-       }) 
+
+        client.messages.create({
+         body: text,
+         messagingServiceSid: 'MG216d22c854ec229d65cd09060464e761',
+         to: '+65'+phno
+       })
 
       .then(message => console.log(message.sid))
       .done();
@@ -162,11 +185,36 @@ app.get("/api/sms/:phno/:name/:val",(req,res)=>
         return res.json({
             status:400,
             success:false,
-	    
+
         });
     }
 
 }
 )
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      // Capture all 404 and 500 errors
+      if (error.status === 404 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 
 app.listen(3000);
