af9fa5ad5defe1f4500449331d7cf495b34088d7
Max Ong Zong Bao <max@codesurance.com>
2022-10-05 12:22:20 +0800

    chore: Renamed redeploy.sh to redeploy_client.sh. Added the redeploy_server.sh for restarting the server.

---
+++ redeploy_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Execute redeployment script"
+echo "Changing directory to mbsProject folder"
+cd ~/mbsProject
+echo "Pullig git from main repo"
+git pull origin main
+echo "Copy current version of DB to backup folder"
+cp ~/mbsProject/Server/Database/mbsProjet.db ~/mbsProject/backup/
+echo "Restarting backend server APIs"
+pm2 restart 0
+echo "Script has been completed!!!"
