e0be820f04be2c49f612d3738c17350eae3f0869
Max Ong Zong Bao <max@codesurance.com>
2022-10-06 11:54:38 +0800

    fix: Twilio not founded in package.json. Updated the redeploy_server.sh to include installing of npm packages.

--- redeploy_server.sh
+++ redeploy_server.sh
@@ -7,6 +7,10 @@ echo "Pullig git from main repo"
 git pull origin main
 echo "Copy current version of DB to backup folder"
 cp ~/mbsProject/Server/Database/mbsProjet.db ~/mbsProject/backup/
+echo "NPM install"
+cd ~/mbsProject/Server/
+npm install
+cd ~/mbsProject
 echo "Restarting backend server APIs"
 pm2 restart 0
 echo "Script has been completed!!!"
