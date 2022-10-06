68f8d8930da9964f687cc0d36074823ed070d98e
Max Ong Zong Bao <max@codesurance.com>
2022-10-06 10:29:00 +0800

    feat: Added the check_tm_number function to check for tm_number.

--- Client/index.js
+++ Client/index.js
@@ -1,5 +1,24 @@
 function pass() {
     let tm_number = document.getElementById('tm_numebr').value;
     localStorage.setItem("tm_number", tm_number)
-    window.location.href = "mbs_registration.html";
+    check_tm_number(tm_number)
+}
+
+function check_tm_number(tm_number) {
+    fetch(`https://onepartyonembs.com.sg/api/info/${tm_number}`,
+        {
+            method: "GET",
+            header: {"Content-type": "application/json;charset=UTF-8"}
+        })
+        .then(response => response.json())
+        .then(json => {
+                console.log(json)
+                window.location.href = "mbs_registration.html";
+            }
+        )
+        .catch(
+            (error) => {
+                console.error('Error:', error);
+                window.location.href = "https://onepartyonembs.com.sg/";
+            });
 }
