5e6c350d673b0658f4cf16fba575ec8f86473623
kulakaka <2003565E@Student.tp.edu.sg>
2022-10-07 12:31:47 +0800

    update selection result to baserow
    Signed-off-by: kulakaka <2003565E@Student.tp.edu.sg>

--- Server/db.js
+++ Server/db.js
@@ -1,38 +1,46 @@
-const sqlite3 = require('sqlite3').verbose();
+// const sqlite3 = require('sqlite3').verbose();
 
 
-const db = new sqlite3.Database('./Server/Database/mbsProjet.db', (err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Connected to database.');
-  });
+// const db = new sqlite3.Database('./Server/Database/mbsProjet.db', (err) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     console.log('Connected to database.');
+//   });
 
 
-// const sql = "INSERT INTO stuffs(tmid,name,department,email,contact) VALUES (?,?,?,?,?)";
+// // const sql = "INSERT INTO stuffs(tmid,name,department,email,contact) VALUES (?,?,?,?,?)";
 
-// db.run(sql,[10010,"Simon Fan","HUMAN RESOURCES","simonfans0928@gmail.com","84600021"],(err)=>
-// {
-//   if (err) return console.error(err.message);
-//   console.log("A new row has been created");
+// // db.run(sql,[10010,"Simon Fan","HUMAN RESOURCES","simonfans0928@gmail.com","84600021"],(err)=>
+// // {
+// //   if (err) return console.error(err.message);
+// //   console.log("A new row has been created");
+// // })
+
+
+// const sql = "SELECT * FROM stuffs";
+// //const sql = "UPDATE stuffs SET selection='1' WHERE tmid=10010;"
+// db.all(sql,[],(err,row)=>{
+//   if(err) return console.error(err.message);
+//   row.forEach((row)=>{
+//     console.log(row);  
+//   });
+// });
+
+//   db.close((err) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     console.log('Close the database connection.');
+//   });
+
+//const axios = require('axios').default;
+
+// axios({
+//   method: "GET",
+//   url: "https://api.baserow.io/api/database/rows/table/104714/?user_field_names=true",
+//   headers: {
+//     Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
+//   }
 // })
 
-
-const sql = "SELECT * FROM stuffs";
-//const sql = "UPDATE stuffs SET selection='1' WHERE tmid=10010;"
-db.all(sql,[],(err,row)=>{
-  if(err) return console.error(err.message);
-  row.forEach((row)=>{
-    console.log(row);  
-  });
-});
-
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Close the database connection.');
-  });
-
-
-  
