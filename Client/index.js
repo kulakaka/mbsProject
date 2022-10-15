42982af34a2b11138eb1cb5b228a4e4c2ac0e713
kulakaka <2003565E@Student.tp.edu.sg>
2022-10-15 20:50:01 +0800

    turn on check number function
    Signed-off-by: kulakaka <2003565E@Student.tp.edu.sg>

--- Client/index.js
+++ Client/index.js
@@ -2,7 +2,7 @@ function pass() {
     let tm_number = document.getElementById('tm_numebr').value;
     localStorage.setItem("tm_number", tm_number)
     window.location.href = "mbs_registration.html";
-    //check_tm_number(tm_number)
+    check_tm_number(tm_number)
 }
   
 
