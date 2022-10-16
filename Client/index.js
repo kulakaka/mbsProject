af80e03d97ab12dff22a1e29ac339d5a7a9233b3
kulakaka <2003565E@Student.tp.edu.sg>
2022-10-16 15:23:58 +0800

    fixing check_tm_number function
    Signed-off-by: kulakaka <2003565E@Student.tp.edu.sg>

--- Client/index.js
+++ Client/index.js
@@ -1,7 +1,6 @@
 function pass() {
     let tm_number = document.getElementById('tm_numebr').value;
     localStorage.setItem("tm_number", tm_number)
-    window.location.href = "mbs_registration.html";
     check_tm_number(tm_number)
 }
   
