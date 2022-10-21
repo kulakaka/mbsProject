ce5596ad66ad3167f5f74816ed301f25f3d38ec9
kulakaka <2003565E@Student.tp.edu.sg>
2022-10-21 10:04:01 +0800

    change api table
    Signed-off-by: kulakaka <2003565E@Student.tp.edu.sg>

--- Server/getData.js
+++ Server/getData.js
@@ -136,8 +136,8 @@ app.get("/api/info/:nm", (req, res) => {
         axios({
             method: "GET",
             //use staffinfo table for retreive data.
-            //url: `https://api.baserow.io/api/database/rows/table/104714/?user_field_names=true&filter__field_656863__contains=${nm}`,
-            url: `https://api.baserow.io/api/database/rows/table/108961/?user_field_names=true&filter__field_687023__contains=${nm}`,
+            url: `https://api.baserow.io/api/database/rows/table/104714/?user_field_names=true&filter__field_656863__contains=${nm}`,
+            //url: `https://api.baserow.io/api/database/rows/table/108961/?user_field_names=true&filter__field_687023__contains=${nm}`,
             headers: {
               Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
             }
@@ -150,8 +150,8 @@ app.get("/api/info/:nm", (req, res) => {
                         // get user info from baserow
                 axios({
                     method: "GET",
-                    //url: `https://api.baserow.io/api/database/rows/table/104714/${rowid}/?user_field_names=true`,
-                    url: `https://api.baserow.io/api/database/rows/table/108961/${rowid}/?user_field_names=true`,
+                    url: `https://api.baserow.io/api/database/rows/table/104714/${rowid}/?user_field_names=true`,
+                    //url: `https://api.baserow.io/api/database/rows/table/108961/${rowid}/?user_field_names=true`,
                     headers: {
                         Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
                     }
