cc0f80659ea8b44f04947221cf485e40cbc7b2bd
kulakaka <simonfans0928@gmail.com>
2022-10-28 09:39:54 +0800

    add check in & redemption api
    Signed-off-by: kulakaka <simonfans0928@gmail.com>

--- Client/mbs_registration.js
+++ Client/mbs_registration.js
@@ -11,83 +11,7 @@ function searchinfo(nm)
 {
     var tm = nm;
 
-    //Get data from local server database
-
-    // fetch(`https://onepartyonembs.com.sg/api/info/${nm}`,
-    // {
-    //     method:"GET",
-    //     header:{"Content-type": "application/json;charset=UTF-8"}
-    // })
-    //     .then(response=>response.json())
-    //     .then(json => {
-    //         console.log(json)
-    //         var name = json[0].Name;
-    //         var Department = json[0].DepartmentName;
-    //         var email = json[0].Email;
-    //         var tm = json[0].TeamMember;
-    //         var contact = json[0].PhoneNo;
-    //         var selection = json[0].SelectedSession;
-
-    //         document.getElementById("name").setAttribute('value',name)
-    //         document.getElementById('Department').setAttribute('value',Department)
-    //         document.getElementById('email').setAttribute('value',email)
-    //         document.getElementById('tm_numebr').setAttribute('value',tm)
-    //         document.getElementById('contact_no').setAttribute('value',contact)
-    //         radiobtn1 = document.getElementById("session1");
-    //         radiobtn2 = document.getElementById("session2");
-
-    //         if (selection == "1"){
-
-    //             radiobtn1.checked = true;
-
-
-    //         }
-    //         if (selection == "2"){
-    //             radiobtn2.checked = true;
-
-
-    //         }
-
-    //     })
-    //     .catch(err=>console.log('Request Failed',err));
-
-        
-
-            // fetch(`https://onepartyonembs.com.sg/api/info/${nm}`,
-            // {
-            //     method:"GET",
-            //     header:{"Content-type": "application/json;charset=UTF-8"}
-            // })
-            // .then(response=>response.json())
-            // .then(json=>{
-            
-            //     //console.log(json)
-            //     // var name = json.Name;
-            //     //var Department = json.DepartmentName;
-            //     // var email = json.Email;
-            //     var tm = json.TeamMember;
-            //     // var contact = json.PhoneNo;
-            //     var selection = json.SelectedSession;
-
-            //     //document.getElementById("name").setAttribute('value',name)
-            //     //document.getElementById('Department').setAttribute('value',Department)
-            //     //document.getElementById('email').setAttribute('value',email)
-            //     document.getElementById('tm_numebr').setAttribute('value',tm)
-            //     //document.getElementById('contact_no').setAttribute('value',contact)
-            //     radiobtn1 = document.getElementById("session1");
-            //     radiobtn2 = document.getElementById("session2");
-
-            //     if (selection == "1"){
-
-            //         radiobtn1.checked = true;
-            //     }
-            //     if (selection == "2"){
-            //         radiobtn2.checked = true;
-            //     }     
-            // })
-            // .catch(err=>console.log(err));
-
-            document.getElementById('tm_numebr').setAttribute('value',tm)
+    document.getElementById('tm_numebr').setAttribute('value',tm)
 }
 
         
@@ -103,25 +27,7 @@ function confrimReg(){
     var phno = document.getElementById("contact_no").value
     var name = document.getElementById("name").value
 
-    //console.log(val);
-
-    //console.log("seletion:"+val);
-
-    //update admin panel api
-    //get rowid
-    
-
-    // fetch(`https://api.baserow.io/api/database/rows/table/104714/?user_field_names=true&filter__field_656863__contains=${tm_nm}`,
-    // {
-    //     method:"GET",
-    //     headers:{"Authorization":"Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"}
-    // })
-    //     .then(response=>response.json())
-    //     .then(json => {
-    //         console.log(json)
-    //         var results = json.results[0];
-    //         rowid = results.id
-    //         console.log("this is id ",rowid)
+  
 
     if(phno.length==0)
     {
@@ -143,10 +49,7 @@ function confrimReg(){
         {
             session_timeslot = "(6:00pm to 12:00am)";
         }
-    
-
-            // send email function
-            
+            // send email function   
         var params = {
             stuff_name :document.getElementById("name").value,
             stuff_email:document.getElementById("email").value,
--- Server/db.js
+++ Server/db.js
@@ -1,166 +1,283 @@
-// API documentation
+// // API documentation
 
-// GET USER INFO 
+const axios = require('axios').default;
 
 
+// // API Registeration
+// // GET USER INFO 
 
-//get request 
+// app.get("/api/info/:nm", (req, res) => {
 
-app.get("/api/info/:nm", (req, res) => {
-
-    var nm = parseInt(req.params.nm);
-    var rowid;
-    console.log("getinfo api")
-    try {
+//     var nm = parseInt(req.params.nm);
+//     var rowid;
+//     console.log("getinfo api")
+//     try {
       
-        //get rowid from baserow
+//         //get rowid from baserow
+//         axios({
+//             method: "GET",
+//             //use staffinfo table for retreive data.
+//             url: `https://api.baserow.io/api/database/rows/table/108961/?user_field_names=true&filter__field_687023__contains=${nm}`,
+//             headers: {
+//               Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
+//             }
+//           })
+//             .then(json => {
+//                   return res.json(json.data.results[0])
+      
+//             })
+//             .catch(err=>{
+//                 console.log('Request Failed',err)
+//                 return res.json("Not Match from server")
+        
+//         }); 
+
+// }
+
+//     catch (error) {
+//         return res.json({
+//             status: 400,
+//             success: false,
+//         });
+//     }
+// })
+
+
+
+// //post request with phno 
+// app.post("/api/update/:id/:selection/:phno/:dep/:email/:name", (req, res) => {
+
+//         var id = parseInt(req.params.id);
+//         var selection = req.params.selection;
+//         var phno = req.params.phno;
+//         var dep = req.params.dep;
+//         var email = req.params.email;
+//         var name = req.params.name;
+//         var rowid;
+//         console.log(id);
+//         console.log(selection);
+        
+//         try {
+
+//             axios({
+//                 method: "GET",
+//                 url: `https://api.baserow.io/api/database/rows/table/109032/?user_field_names=true&filter__field_687456__contains=${id}`,
+//                 headers: {
+//                   Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
+//                 }
+//               })
+//                 .then(json => {
+//                     //console.log(json)
+//                     var rowid = json.data.results[0].id
+//                     // update selection baserow for admin panel 
+//                          axios({
+//                              method: "PATCH",
+//                              url: `https://api.baserow.io/api/database/rows/table/109032/${rowid}/?user_field_names=true`,
+             
+//                              headers: {
+//                              Authorization : "Token pJUmXlCIRJaP618ys13YJDdrvi3DUAGq",
+//                              "Content-Type": "application/json"
+//                              },
+//                              data: {
+//                                  "SelectedSession": selection,
+//                                  "Email":email,
+//                                  "PhoneNo":phno,
+//                                  "Name":name,
+//                                  "Department":dep,
+//                                  "TeamMember":id
+                                 
+//                              }
+//                          })
+                
+//                      }
+                
+                
+//                 )
+//                 .catch(err=>{                  
+//                     axios({
+//                         method: "POST",
+//                         url: "https://api.baserow.io/api/database/rows/table/109032/?user_field_names=true",
+//                         headers: {
+//                           Authorization: "Token pJUmXlCIRJaP618ys13YJDdrvi3DUAGq",
+//                           "Content-Type": "application/json"
+//                         },
+//                         data: {
+//                           "TeamMember": id,
+//                           "Name": name,
+//                           "Department": dep,
+//                           "Email": email,
+//                           "PhoneNo": phno,
+//                           "SelectedSession": selection
+//                         }
+//                       })
+// });
+
+//         } catch (error) {
+//             return res.json({
+//                 status: 400,
+//                 success: false,
+//             });
+//         }
+        
+//     }
+// )
+
+
+// // check if is valid user from register
+
+// app.get("/api/info/:nm", (req, res) => {
+
+//     var nm = parseInt(req.params.nm);
+//     var rowid;
+//     console.log("getinfo api")
+//     try {
+      
+//         //get rowid from baserow
+//         axios({
+//             method: "GET",
+//             //use staffinfo table for retreive data.
+//             url: `https://api.baserow.io/api/database/rows/table/108961/?user_field_names=true&filter__field_687023__contains=${nm}`,
+//             headers: {
+//               Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
+//             }
+//           })
+//             .then(json => {
+//                   return res.json(json.data.results[0])
+      
+//             })
+//             .catch(err=>{
+//                 console.log('Request Failed',err)
+//                 return res.json("Not Match from server")
+        
+//         }); 
+
+// }
+
+//     catch (error) {
+//         return res.json({
+//             status: 400,
+//             success: false,
+//         });
+//     }
+// })
+
+
+
+// // API for check in 
+// var nm = 844000;
+// axios({
+//     method: "GET",
+//     //use staffinfo table for retreive data.
+//     url: `https://api.baserow.io/api/database/rows/table/110074/?user_field_names=true&filter__field_695169__contains=${nm}`,
+//     headers: {
+//         Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
+//     }
+//     })
+//     .then(json => {
+//            //console.log(json.data.results[0])
+//         let output = json.data.results[0]
+//         let tm = output.TeamMember;
+//         let name = output.Name;
+//         let dep = output.Department;
+//         let phno = output.PhoneNo;
+//         let email = output.Email;
+
+//     axios({
+//         method: "POST",
+//         url: "https://api.baserow.io/api/database/rows/table/109802/?user_field_names=true",
+//         headers: {
+//           Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua",
+//           "Content-Type": "application/json"
+//         },
+//         data: {
+//           "TeamMember": tm,
+//           "Name": name,
+//           "Department": dep,
+//           "Email": email,
+//           "PhoneNo":phno,
+//         }
+//       })
+
+//     })
+//     .catch(err=>{
+//         console.log('Unfind in database',err)
+//         //return res.json("Not Match from server")
+//     })
+
+
+
+// API for Redemption
+//check vaild user from checked in
+
+axios({
+    method: "GET",
+    url: `https://api.baserow.io/api/database/rows/table/109032/?user_field_names=true&filter__field_687456__contains=${nm}`,
+    headers: {
+        Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
+    }
+    })
+    .then(json => {
+        const date = new Date();
+
+        // check if is exising user in redemption table
         axios({
             method: "GET",
-            //use staffinfo table for retreive data.
-            url: `https://api.baserow.io/api/database/rows/table/108961/?user_field_names=true&filter__field_687023__contains=${nm}`,
+            url: `https://api.baserow.io/api/database/rows/table/109803/?user_field_names=true&filter__field_692441__contains=${nm}`,
             headers: {
-              Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
+            Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
             }
-          })
-            .then(json => {
-                  return res.json(json.data.results[0])
-      
-            })
-            .catch(err=>{
-                console.log('Request Failed',err)
-                return res.json("Not Match from server")
-        
-        }); 
-
-}
-
-    catch (error) {
-        return res.json({
-            status: 400,
-            success: false,
-        });
-    }
-})
-
-
-
-
-//post request with phno 
-app.post("/api/update/:id/:selection/:phno/:dep/:email/:name", (req, res) => {
-
-        var id = parseInt(req.params.id);
-        var selection = req.params.selection;
-        var phno = req.params.phno;
-        var dep = req.params.dep;
-        var email = req.params.email;
-        var name = req.params.name;
-        var rowid;
-        console.log(id);
-        console.log(selection);
-        
-        try {
+        })
+        .then(json=>{
+            if (json.data.results[0].FristDrink==true &&json.data.results[0].SecondDrink==true )
+            {
+                return res.json("Your have been fully redeemed")
+            }
+            else{
 
             axios({
-                method: "GET",
-                url: `https://api.baserow.io/api/database/rows/table/109032/?user_field_names=true&filter__field_687456__contains=${id}`,
+                method: "PATCH",
+                url: `https://api.baserow.io/api/database/rows/table/109803/?user_field_names=true&filter__field_692441__contains=${nm}`,
                 headers: {
-                  Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
+                  Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua",
+                  "Content-Type": "application/json"
+                },
+                data: {
+                  "TeamMember": nm,
+                  "Name": name,
+                  "SecondDrink": true,
+                  "SecondDrinkTime": date
+            
                 }
               })
-                .then(json => {
-                    //console.log(json)
-                    var rowid = json.data.results[0].id
-                    // update selection baserow for admin panel 
-                         axios({
-                             method: "PATCH",
-                             url: `https://api.baserow.io/api/database/rows/table/109032/${rowid}/?user_field_names=true`,
-             
-                             headers: {
-                             Authorization : "Token pJUmXlCIRJaP618ys13YJDdrvi3DUAGq",
-                             "Content-Type": "application/json"
-                             },
-                             data: {
-                                 "SelectedSession": selection,
-                                 "Email":email,
-                                 "PhoneNo":phno,
-                                 "Name":name,
-                                 "Department":dep,
-                                 "TeamMember":id
-                                 
-                             }
-                         })
-                
-                     }
-                
-                
-                )
-                .catch(err=>{                  
-                    axios({
-                        method: "POST",
-                        url: "https://api.baserow.io/api/database/rows/table/109032/?user_field_names=true",
-                        headers: {
-                          Authorization: "Token pJUmXlCIRJaP618ys13YJDdrvi3DUAGq",
-                          "Content-Type": "application/json"
-                        },
-                        data: {
-                          "TeamMember": id,
-                          "Name": name,
-                          "Department": dep,
-                          "Email": email,
-                          "PhoneNo": phno,
-                          "SelectedSession": selection
-                        }
-                      })
-});
-
-        } catch (error) {
-            return res.json({
-                status: 400,
-                success: false,
-            });
-        }
-        
-    }
-)
-
-
-// check if is valid user from register
-
-
-
-
-app.get("/api/info/:nm", (req, res) => {
-
-    var nm = parseInt(req.params.nm);
-    var rowid;
-    console.log("getinfo api")
-    try {
-      
-        //get rowid from baserow
-        axios({
-            method: "GET",
-            //use staffinfo table for retreive data.
-            url: `https://api.baserow.io/api/database/rows/table/108961/?user_field_names=true&filter__field_687023__contains=${nm}`,
-            headers: {
-              Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua"
             }
-          })
-            .then(json => {
-                  return res.json(json.data.results[0])
-      
-            })
-            .catch(err=>{
-                console.log('Request Failed',err)
-                return res.json("Not Match from server")
-        
-        }); 
 
-}
 
-    catch (error) {
-        return res.json({
-            status: 400,
-            success: false,
-        });
-    }
-})
+        })
+        .catch(err=>{
+            axios({
+                method: "POST",
+                url: "https://api.baserow.io/api/database/rows/table/109803/?user_field_names=true",
+                headers: {
+                  Authorization: "Token GJTONGLhbwvH8cxVXGrcY5PVM323aZua",
+                  "Content-Type": "application/json"
+                },
+                data: {
+                  "TeamMember": nm,
+                  "Name": name,
+                  "FristDrink":true,
+                  "FirstDrinkTime":date
+                  
+                }
+              })
+
+        })
+
+ 
+
+    })
+    .catch(err=>{
+        console.log('Request Failed',err)
+        return res.json("Not Match from server")
+
+}); 
+
