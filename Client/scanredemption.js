34b90ff7e854f422b9d2aa726f0b8ac9ff382167
kulakaka <simonfans0928@gmail.com>
2022-11-21 12:57:07 +0800

    test
    Signed-off-by: kulakaka <simonfans0928@gmail.com>

--- Client/scanredemption.js
+++ Client/scanredemption.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', (ev)=>{
                 method:"PUT",
                 body:formData
             })
-            .then((response) => {console.log(response)})
+            .then((response) =>response.text())
             .then((result) => {
                 console.log('Success:',result);
                 if(result.success)
